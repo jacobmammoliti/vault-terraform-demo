a125b18a46cee8d5a00ecd20b1c7a3871cdd1c03
Jacob Mammoliti <jacob.mammoliti@arctiq.ca>
2020-07-28 13:55:39 -0400

    updating k8s provider

--- terraform-vault-deployment/provider.tf
+++ terraform-vault-deployment/provider.tf
@@ -12,9 +12,11 @@ provider "kubernetes" {
 }
 
 provider "helm" {
+
   kubernetes {
-    host  = google_container_cluster.kubernetes_cluster.endpoint
-    token = data.google_client_config.default.access_token
+    load_config_file = "false"
+    host             = google_container_cluster.kubernetes_cluster.endpoint
+    token            = data.google_client_config.default.access_token
 
     cluster_ca_certificate = base64decode(
       google_container_cluster.kubernetes_cluster.master_auth.0.cluster_ca_certificate
