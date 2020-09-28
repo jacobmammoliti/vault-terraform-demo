b03fa6624d9a612fc5d5392c32c96018adf90ace
Jacob Mammoliti <jacob.mammoliti@arctiq.ca>
2020-09-28 17:22:05 -0400

    removing sa

--- terraform-vault-deployment/gke.tf
+++ terraform-vault-deployment/gke.tf
@@ -1,15 +1,3 @@
-resource "google_service_account" "gke_service_account" {
-  project      = var.project_id
-  account_id   = var.gke_service_account_id
-  display_name = "Service Account for Kubernetes Cluster"
-}
-
-resource "google_service_account_iam_member" "admin-account-iam" {
-  service_account_id = google_service_account.gke_service_account.name
-  role               = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-  member             = "serviceAccount:${google_service_account.gke_service_account.email}"
-}
-
 resource "google_container_cluster" "kubernetes_cluster" {
   name     = var.cluster_name
   location = var.location
@@ -22,11 +10,10 @@ resource "google_container_cluster" "kubernetes_cluster" {
     oauth_scopes = var.oauth_scopes
 
     metadata = {
-      disable-legacy-endpoints = "true"
+      disable-legacy-endpoints = true
     }
 
     machine_type    = var.machine_type
     preemptible     = var.preemptible
-    service_account = google_service_account.gke_service_account.email
   }
 }
