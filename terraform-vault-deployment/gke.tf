3d55b0192f7a2eaa226be717f3cb8932916e419b
Jacob Mammoliti <jacob.mammoliti@arctiq.ca>
2020-09-28 17:09:39 +0000

    created designated SA

--- terraform-vault-deployment/gke.tf
+++ terraform-vault-deployment/gke.tf
@@ -1,3 +1,14 @@
+resource "google_service_account" "gke_service_account" {
+  account_id   = var.gke_service_account_id
+  display_name = "Service Account for Kubernetes Cluster"
+}
+
+resource "google_service_account_iam_member" "admin-account-iam" {
+  service_account_id = google_service_account.gke_service_account.name
+  role               = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+  member             = "serviceAccount:${google_service_account.gke_service_account.email}"
+}
+
 resource "google_container_cluster" "kubernetes_cluster" {
   name     = var.cluster_name
   location = var.location
@@ -15,5 +26,6 @@ resource "google_container_cluster" "kubernetes_cluster" {
 
     machine_type = var.machine_type
     preemptible  = var.preemptible
+    service_account = google_service_account.gke_service_account.email
   }
 }
