8e733155823816cf16b22a9d600ce7e8a85807ea
Jacob Mammoliti <jacob.mammoliti@arctiq.ca>
2020-09-28 18:24:41 +0000

    removing count for tls for now

--- terraform-vault-deployment/tls.tf
+++ terraform-vault-deployment/tls.tf
@@ -1,12 +1,10 @@
 resource "tls_private_key" "cert" {
-  count       = var.vault_tls_disable == true ? 0 : 1
   algorithm   = "RSA"
   ecdsa_curve = "P384"
   rsa_bits    = "2048"
 }
 
 resource "tls_self_signed_cert" "primary_cert" {
-  count           = var.vault_tls_disable == true ? 0 : 1
   key_algorithm   = tls_private_key.cert.algorithm
   private_key_pem = tls_private_key.cert.private_key_pem
 
